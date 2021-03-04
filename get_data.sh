9a4224feaf523ecd348be87caccdfcfec0b22c22
Ashraf GHIYE <ashrafghiye@gmail.com>
2021-03-04 03:06:03 +0100

    Update get_data.sh

--- get_data.sh
+++ get_data.sh
@@ -7,3 +7,10 @@ gdown "https://drive.google.com/uc?id=1G3aCGuKWLJr6kiF_eb2CzLhoq-DHLzlF"
 gdown "https://drive.google.com/uc?id=1ahIDcG3CNUcAwOOy9iWx_NDcFErD5Lnn"
 gdown "https://drive.google.com/uc?id=1-G5KjbCUcBcyPzLpsjEyBL3E0Tqwfi6E"
 gdown "https://drive.google.com/uc?id=1oOjMApxN4OZZFPSHN56u9M9sqGantJi5"
+
+
+python weighted_graph.py
+python abstracts_word_counts.py
+
+### you can download a 64 Doc2Vec embeddings with the above link, also here is the script to re-generate them.
+#python paper_representations_64.py
