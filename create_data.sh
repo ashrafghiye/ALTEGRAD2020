58e8920299604449152932a591bbd02ac3b61975
jawadhussein462 <41950044+jawadhussein462@users.noreply.github.com>
2021-03-04 03:01:48 +0100

    Add files via upload

---
+++ create_data.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+gdown "https://drive.google.com/uc?id=1xFnj04M6GxTgjURDuuKC2VrGky0qxyB2"
+gdown "https://drive.google.com/uc?id=1e1FQa4BD1FaaiFVlGfzW46QRP-PiN5pK"
+gdown "https://drive.google.com/uc?id=1wc4rQt8UPwkLSB5IqeRIFGBZxAkBU2kd"
+gdown "https://drive.google.com/uc?id=1G3aCGuKWLJr6kiF_eb2CzLhoq-DHLzlF"
+gdown "https://drive.google.com/uc?id=1ahIDcG3CNUcAwOOy9iWx_NDcFErD5Lnn"
+gdown "https://drive.google.com/uc?id=1-G5KjbCUcBcyPzLpsjEyBL3E0Tqwfi6E"
+gdown "https://drive.google.com/uc?id=1oOjMApxN4OZZFPSHN56u9M9sqGantJi5"
